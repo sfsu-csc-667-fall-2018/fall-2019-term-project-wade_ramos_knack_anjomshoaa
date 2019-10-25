6b1bed48c9453680723f567266fb248d05e18836
Shane Wade <shane.au.wade@gmail.com>
2019-10-25 15:41:42 -0700

    created postgres.js file that holds a pg-promise object.  That allows us to use one database connector instance for the entire program.I additionally added two more api endpoints.  We can now create a brand new gamestate, get an exisiting gamestate, and also update an already exisiting gamestate

---
+++ postgres.js
@@ -0,0 +1,24 @@
+const pgp = require('pg-promise')();
+
+const connection = {
+    host: process.env.DB_HOST,
+    port: 5432,
+    database: process.env.DB_NAME,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS
+};
+
+const db = pgp(connection);
+
+// db.any('SELECT * FROM gamestates where uuid = \'13b0f464-cd17-4e66-b002-f06bcf0f36ff\'', [true])
+//     .then(function(data) {
+//         // success;
+//         console.table(data);
+//     })
+//     .catch(function(error) {
+//         // error;
+//         //console.log('error in the db or query')
+//         console.error(error)
+//     });
+
+module.exports = db;
