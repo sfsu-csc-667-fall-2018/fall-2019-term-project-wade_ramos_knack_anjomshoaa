e84acec786fcd55ad47b64440d2e351c48b78f0e
Jonathon Knack <jknack@mail.sfsu.edu>
2019-11-11 22:35:41 -0800

    login & register done

--- app.js
+++ app.js
@@ -10,10 +10,12 @@ if(process.env.NODE_ENV === 'development') {
   require('dotenv').config();
 }
 
+
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 const gameRouter = require('./routes/game')
 const testRouter = require('./routes/test')
+
 
 const app = express();
 
@@ -21,11 +23,12 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static("./build"))
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
---
+++ build/precache-manifest.c81466c53d4489d1841e8b7e29c2be25.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57361788b0392b9e8fe83fc3082fd014",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a684a3d776ff4af8251d",
+    "url": "/static/css/main.8d7228ce.chunk.css"
+  },
+  {
+    "revision": "66eca656e273048f47e3",
+    "url": "/static/js/2.d43262da.chunk.js"
+  },
+  {
+    "revision": "a684a3d776ff4af8251d",
+    "url": "/static/js/main.18692d30.chunk.js"
+  },
+  {
+    "revision": "33de3d9682f72931a2b9",
+    "url": "/static/js/runtime-main.fcdc870b.js"
+  }
+]);
