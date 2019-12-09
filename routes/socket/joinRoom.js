9f233215abb8e37a112170f92b7196e3ebbb068d
Amir <ar.anjomshoaa@gmail.com>
2019-12-08 16:38:10 -0800

    Socket conf

---
+++ routes/socket/joinRoom.js
@@ -0,0 +1,3 @@
+const joinLobby = (io,socket) => {
+    
+}
--- routes/socket/socketServer.js
+++ routes/socket/socketServer.js
@@ -2,17 +2,40 @@ const io = require('socket.io')(3002,{
     cookie: true
   })
 
+  let users = [{
+      socketId: '',
+      user: ''
+  }]
+
 
  io.on('connection', socket => {
       console.log("Connected")
-      socket.join('lobby')
+      const socketId = socket.id
+      socket.on('join',room => {
+          console.log("Join")
+          console.log(room)
+          socket.join(room)
+      })
 
       socket.on('income-msg',({msg,user}) => {
         
         console.log(msg)
         console.log(user)
+        users.push({socketId,user})
+        
         io.to('lobby').emit('chat',{incomeMsg: msg, user:user})
     })  
+    console.log(users)
+
+    socket.on('disconnect', () => {
+        console.log('Disconnected')
+        users.forEach(user => {
+            if(user.socketId === socket.id){
+                users.splice(users.indexOf(user),1)
+            }
+        })
+        console.log(users)
+    })
 
 
 
